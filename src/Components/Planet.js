5539327947eb8d9e53e26e027631a30cfa0f314b
Bonnary <69995568+Bonnary@users.noreply.github.com>
2021-11-08 19:03:08 +0700

    3d object

--- src/Components/Planet.js
+++ src/Components/Planet.js
@@ -5,34 +5,51 @@ import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import moon from "../assets/3D-texture/moon.jpg";
 
-const Object = async ({ position }) => {
+const Box = () => {
   const mesh = useRef(false);
   const texture = useLoader(THREE.TextureLoader, moon);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.005));
   return (
-    <mesh position={position} ref={mesh}>
+    <mesh castShadow ref={mesh}>
       <dodecahedronGeometry args={[2, 100]} />
       <meshBasicMaterial attach="material" map={texture} />
     </mesh>
   );
 };
 
-
-export default function Planet() {
+function Planet() {
   return (
     <Container>
-      <Canvas>
-        {/* <ambientLight intensity={0.3} />
+      <Canvas
+        colorManagement
+        shadowMap
+        camera={{ fov: 30, position: [-5, 2, 10] }}
+      >
+        <directionalLight
+          castShadow
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        <ambientLight intensity={0.3} />
         <pointLight position={[-10, 0, -20]} intensity={0.5} />
-        <pointLight position={[0, -10, 0]} intensity={1.5} /> */}
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
         <Suspense fallback={null}>
-          <Object position={[0, 1, 0]} />
+          <Box />
           <OrbitControls />
         </Suspense>
       </Canvas>
     </Container>
   );
 }
+
+export default Planet;
 
 const Container = styled.div`
   width: 100%;
